76ecfdea895f7716db1a751f9c4dbbf4f7a721b5
Jam <jam29n@gmail.com>
2024-05-25 17:42:58 +0200

    html2text01

---
+++ node_modules/html-base/babel.config.js
@@ -0,0 +1,12 @@
+export default {
+  plugins: ['@babel/plugin-proposal-class-properties'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'entry',
+        corejs: 3
+      }
+    ]
+  ]
+};
